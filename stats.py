122a60044714c6e8da6e3f2124b00b79ca4d2537
SVS <stepanezc@gmail.com>
2022-10-02 16:14:37 +0300

    Update docs; And fields for result

--- stats.py
+++ stats.py
@@ -1,5 +1,4 @@
 ﻿import csv
-from datetime import datetime, timedelta
 import argparse
 import os
 
@@ -8,9 +7,8 @@ parser.add_argument("--source-file", "-s", required=True, help="Path to CSV file
 parser.add_argument("--result-file", "-e", required=True, help="Path to CSV file with end time values", type=str)
 parser.add_argument("--new-source-file", "-sn", required=True, help="Path to new CSV file with start time values", type=str)
 parser.add_argument("--new-result-file", "-en", required=True, help="Path to new CSV file with end time values", type=str)
-#parser.add_argument("--time", "-t", help="Timestamp parameter. Must be in format \"%Y-%m-%d %H:%M:%S %Z\". Example 2022-09-23 14:02:41 UTC", required=True, type=str)
+parser.add_argument("--verbose", "-v", required=False, help="Enable verbose. Print all difference in entries", type=bool)
 args = parser.parse_args()
-
                #args.source-file       args.result-file
 def PrepareFile(source_file_path: str, result_file_path: str):
     """
@@ -25,7 +23,7 @@ def PrepareFile(source_file_path: str, result_file_path: str):
 #! ALL source_file_path rename to result_file_path
 
     #"E:\\Downloads\\stats_complete.csv"
-    with open(result_file_path.replace("\\"), "w", encoding='utf-8') as complete_file: #former source_file_path
+    with open(result_file_path, "w", encoding='utf-8') as complete_file: #former source_file_path
         complete_file.write(stats_headers)
 
 
@@ -47,59 +45,78 @@ def PrepareFile(source_file_path: str, result_file_path: str):
             list_users.append(item)
     return list_users
 
-def calc_delta(old_val: int, new_val: int):
+def calc_delta(new_val: int, old_val: int):
+    """
+    Takes two integer values and return their delta
+    """
     return new_val - old_val
 
-def create_result(newer_list: list, older_list: list):
-    result = {}
+def create_result(newer_list: list, older_list: list, **kwargs):
+    """
+    Takes two positional arguments, <newer_list> and <older_list> and one boolean key-word argument for verbosity
+    Performs comparison between them, and if elements are the same add them to the new dict
+    Return the result as a list of the dictionary.
+    Keys in dict: ID, Name, Username, MSG(delta of messages)
+    """
+    result_list = []
+    admins_list = ["qwerty_nana", "lebovsk"]
     for item in newer_list:
         for old_item in  older_list:
-            if item["ID"] != old_item["ID"]:
+            if(item["ID"] != old_item["ID"]) or (item["Username"] in admins_list):
                 continue
             else:
-                result[item["ID"]] = [item["Name"], item["UserName"], calc_delta(int(item["MSG"]) - int(old_item["MSG"]))]
-    return result
+                try:
+                    if(calc_delta(int(item["MSG"].strip()), int(old_item["MSG"].strip())) != 0):
+                        if(kwargs.get("verbose")):
+                            print("New:|","ID:", item["ID"],"Name:",item["Name"], item["Username"], "Time:",item["Last message"],"Old:|", "ID:",old_item["ID"], "Name:",old_item["Name"], old_item["Username"], " Time: ",old_item["Last message"], " MSG: ", calc_delta(int(item["MSG"].strip()), int(old_item["MSG"].strip())))
+                        result_list.append({"ID": item["ID"], "Name": item["Name"], "Username": item["Username"], "MSG": calc_delta(int(item["MSG"].strip()), int(old_item["MSG"].strip())), "From_datetime": old_item["Last message"],"To_datetime": item["Last message"]})
+
+                except ValueError:
+                    if(item["ID"] == "ID"):
+                        pass
+                    else:
+                        print(item["ID"]," - have corrupted fields")
+    return result_list
 
 def find_min_max_count_of_msg(list_users_dicts: list):
-    list_corrupted_users = []
+    print("Total counts of different entries:", len(list_users_dicts))
+    min_val = {"Name": None, "Value": 0, "ID": None}
+    max_val = {"Name": None, "Value": 0, "ID": None}
     for j in range(1, len(list_users_dicts)):
-        try:
-            if (int(list_users_dicts[j]["MSG"].strip()) < min_val["Value"]):
-                min_val["Value"] = int(list_users_dicts[j]["MSG"].strip())
-                min_val["Name"] = list_users_dicts[j]["Name"].strip()
-                min_val["ID"] = list_users_dicts[j]["ID"].strip()
+        if (int(list_users_dicts[j]["MSG"]) < min_val["Value"]):
+            min_val["Value"] = int(list_users_dicts[j]["MSG"])
+            min_val["Name"] = list_users_dicts[j]["Name"].strip()
+            min_val["ID"] = list_users_dicts[j]["ID"].strip()
+            min_val["Start_datetime"] = list_users_dicts[j]["From_datetime"].strip()
+            min_val["Final_datetime"] = list_users_dicts[j]["To_datetime"].strip()
 
-            if (int(list_users_dicts[j]["MSG"].strip()) > max_val["Value"]):
-                max_val["Value"] = int(list_users_dicts[j]["MSG"].strip())
-                max_val["Name"] = list_users_dicts[j]["Name"].strip()
-                max_val["ID"] = list_users_dicts[j]["ID"].strip()
-        except:
-            list_corrupted_users.append(list_users_dicts[j]["Name"].strip())
+        if (int(list_users_dicts[j]["MSG"]) > max_val["Value"]):
+            max_val["Value"] = int(list_users_dicts[j]["MSG"])
+            max_val["Name"] = list_users_dicts[j]["Name"].strip()
+            max_val["ID"] = list_users_dicts[j]["ID"].strip()
+            max_val["Start_datetime"] = list_users_dicts[j]["From_datetime"].strip()
+            max_val["Final_datetime"] = list_users_dicts[j]["To_datetime"].strip()
 
-    print("Users with corrupted fields: ", list_corrupted_users)
-    print("Max: ", max_val["Value"], "\nMin: ", min_val["Value"], "\nWinner: ", max_val["Name"], "\nWinner ID: ", max_val["ID"])
+    print("Max: ", max_val["Value"], "\nMin: ", min_val["Value"], "\nWinner: ", max_val["Name"], "\nWinner ID: ", max_val["ID"], "\nStart datetime: ", max_val["Start_datetime"], "\nFinal datetime:", max_val["Final_datetime"])
     return {max_val["ID"]: [max_val["Name"], max_val["Value"]]}
 
 
 
-min_val = {"Name": None, "Value": 0, "ID": None}
-max_val = {"Name": None, "Value": 0, "ID": None}
-list_users_dict = PrepareFile(args.source_file, args.result_file)
-list_users_dict_new = PrepareFile(args.new_source_file, args.new_result_file)
-result = create_result(list_users_dict_new, list_users_dict)
+
+src_file = "C:\\Users\\stepa\\Documents\\Repositories\\Python\\Stats\\csv\\Cryptonic Чат - users (exported from combot.org).csv"
+res_file = "C:\\Users\\stepa\\Documents\\Repositories\\Python\\Stats\\csv\\stats_complete.csv"
+new_src_file = "C:\\Users\\stepa\\Documents\\Repositories\\Python\\Stats\\csv\\Cryptonic Чат - users (exported from combot.org)(1).csv"
+new_res_file = "C:\\Users\\stepa\\Documents\\Repositories\\Python\\Stats\\csv\\stats_complete_new.csv"
+list_users_dict = PrepareFile(args.source_file, args.result_file) #PrepareFile(src_file, res_file)
+list_users_dict_new = PrepareFile(args.new_source_file, args.new_result_file) #PrepareFile(new_src_file, new_res_file)
+result = create_result(list_users_dict_new, list_users_dict, verbose = args.verbose)
 find_min_max_count_of_msg(result)
-
-
-# time = list_users_dict[1].get("Last message").strip()
-# format_string = "%Y-%m-%d %H:%M:%S %Z"
-# time_start = datetime.strptime(time, format_string)
-# time_end = time_start + timedelta(days=7)
-# print("Last message: ", time, "\nStart time: ", time_start, "\nEnd time: ", time_end)
 os.system("pause")
 
 
-# c:\Users\stepa\Documents\Repositories\Python\Stats\stats.py ^
-# -s "C:\Users\stepa\Documents\Repositories\Python\Stats\csv\Cryptonic Чат - users (exported from combot.org).csv" ^
-# -e "C:\Users\\tepa\Documents\Repositories\Python\Stats\csv\stats_complete.csv" ^
-# -sn "C:\Users\stepa\Documents\Repositories\Python\Stats\csv\Cryptonic Чат - users (exported from combot.org)(1).csv" ^
-# -en "C:\Users\stepa\Documents\Repositories\Python\Stats\csv\stats_complete_new.csv" ^
+# C:\Users\stepa\Documents\Repositories\Python\Stats\stats.py
+# -s "C:\\Users\\stepa\\Documents\\Repositories\\Python\\Stats\\csv\\Cryptonic Чат - users (exported from combot.org).csv" `
+# -e "C:\\Users\\stepa\\Documents\\Repositories\\Python\\Stats\\csv\\stats_complete.csv" `
+# -sn "C:\\Users\\stepa\\Documents\\Repositories\\Python\\Stats\\csv\\Cryptonic Чат - users (exported from combot.org)(1).csv" `
+# -en "C:\\Users\\stepa\\Documents\\Repositories\\Python\\Stats\\csv\\stats_complete_new.csv"
+# -v True
