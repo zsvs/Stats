19dd406746d321ca73913fa96a0ad8f0c24000cc
SVS <stepanezc@gmail.com>
2022-10-02 21:59:08 +0300

    Fixes

--- stats.py
+++ stats.py
@@ -151,7 +151,8 @@ def main():
     Run main program
     """
 
-    parser = argparse.ArgumentParser(description="Calculate winner. All path for window must be like \"C:\\Foo\\Bar\\text.txt\"")
+    parser = argparse.ArgumentParser(
+                        description="Calculate winner. All path for window must be like \"C:\\Foo\\Bar\\text.txt\"")
     parser.add_argument("--source-file",
                         "-s",
                         required=True,
